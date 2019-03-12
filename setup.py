8d96a05a9e1c37e1d6812df1d35f6253f941ab4c
Pavel Dvorkin <dpavel20@gmail.com>
2019-03-12 10:53:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="myapp",
     version="1.0",
+    url='https://github.com/XxdpavelxX/myapp',
     install_requires=[
         "boto3==1.9.48",
         "selenium==3.141.0",
