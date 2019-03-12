d55f2b91f9b9fcc03f32885b8d556c0507821b6d
XxdpavelxX <dpavel20@gmail.com>
2019-03-12 12:20:03 -0400

    Potential fix

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="myapp",
     version="1.0",
+    packages=["myapp"],
     url='https://github.com/XxdpavelxX/myapp',
     install_requires=[
         "boto3",
